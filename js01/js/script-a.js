5d2b7e95c3736d838ea7c70101fe8f2fc4ba0576
Tadaphat-95 <s6752410028@gmail.com>
2025-03-25 16:04:06 +0700

    Study JS1

---
+++ js01/js/script-a.js
@@ -0,0 +1,6 @@
+// Pascal Case , upper Camel Case : FinalScoreDti
+//camel Case , lower Camel Case : finalScoreDti
+//Snake Case : final_score_dti , FINAL_SCORE_DTI
+//Kebab Case : final-score-dti , FINAL-SCORE-DTI (for Web)
+alert('Wow')
+console.log('Welcome to DTI')
